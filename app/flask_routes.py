fda64a0f5d97fb6527f460759da61ccc5c3853f7
grqx_win <user.email>
2025-04-08 14:27:27 +1200

    add a gpu page

--- app/flask_routes.py
+++ app/flask_routes.py
@@ -13,7 +13,16 @@ def index_html():
     return render_template('index.html')
 
 
+def gpu_info_page(gpu_id):
+    return render_template('gpu/template.html', **{
+        'gpu_id': gpu_id,
+        'gpu_name': '<Unavailable>',
+        # TODO: more
+    })
+
+
 def setup_flask_app(app: Flask):
     app.route('/')(index)
     app.route('/index.html')(index_html)
+    app.route('/gpu/<int:gpu_id>')(gpu_info_page)
     return app
---
+++ app/static/gpu/gpuInfo.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", () => {
+    alert("Hello, GPU Info!")
+});
